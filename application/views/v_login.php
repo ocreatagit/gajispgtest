c098adf9dfd91739603ad0abbe3a78107c4c2e87
RRyner <ronald.lloyd57@gmail.com>
2015-12-07 23:21:03 +0700

    HAHAHAHA

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -61,7 +61,7 @@
 
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
-                                
+                                HAHAHAHAHAH
                             </div>
                         </div>
                     </form><br>
